e2e8f7cff0dbe9f7dd27f3ef262c2c72c2f4e9a3
Jesse Chong <jessechong@pursuit.org>
2023-11-08 11:54:45 -0500

    Debug

--- db/seed.sql
+++ db/seed.sql
@@ -4,8 +4,8 @@
 -- cardio (jogging/running, biking, swimming, treadmill), 
 -- weight-training (weight-training machines, free weights), 
 -- flexibility ( yoga, pilates, stretching)  
-Insert INTO workout_tb (workout_name, workout_type, workout_days, is_true) VALUES;
+Insert INTO workout_tb (workout_name, workout_type, workout_days, is_true) VALUES
 ('push-ups/sit-ups', 'calistethics', 3, true),
 ('jogging/running', 'cardio', 5, false),
 ('pilates/yoga/stretching', 'flexibility', 3, true),
-('free weights/weight machines', 'strength building', 2, false),
+('free weights/weight machines', 'strength building', 2, false)
