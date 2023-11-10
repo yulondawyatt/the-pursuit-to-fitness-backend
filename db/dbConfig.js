509e32e1e3b02077667c89d344c21e1ba6c56aa3
yulondawyatt@gmail.com <yulondajoywyatt@Yulondas-MacBook-Air.local>
2023-11-09 19:07:38 -0500

    Still figuring out bugs

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -9,16 +9,16 @@ const cn = {
 };
 
 const db = pgp(cn);
-db.connect()
-  .then((cn) => {
-    const { user, host, port, database } = cn.client;
-    console.log(
-      "\x1b[90m" +
-        `Postgres connection established with user:${user}, host:${host}, port:${port}, database:${database}` +
-        "\x1b[0m"
-    );
-    cn.done();
-  })
-  .catch((error) => console.log("database connection error", error));
+// db.connect()
+//   .then((cn) => {
+//     const { user, host, port, database } = cn.client;
+//     console.log(
+//       "\x1b[90m" +
+//         `Postgres connection established with user:${user}, host:${host}, port:${port}, database:${database}` +
+//         "\x1b[0m"
+//     );
+//     cn.done();
+//   })
+//   .catch((error) => console.log("database connection error", error));
 
 module.exports = db;
