7e2bc4e6669335435b790419e1c33baf520986e5
yulondawyatt@gmail.com <yulondajoywyatt@Yulondas-MacBook-Air.local>
2023-11-16 11:16:29 -0500

    adding password connect object in dbConfig file

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE fitness_db;
 CREATE TABLE workout_tb (
     id SERIAL PRIMARY KEY,
     workout_name TEXT NOT NULL,
-    workout_days INT NOT NULL, -- days commited to excercise
+    workout_days INT NOT NULL, 
     workout_type TEXT,
-    is_true BOOLEAN -- did you complete all commited days?
+    is_true BOOLEAN 
 );
